d74a4e906ced6c7850b8453e48a6fb7dca0fbf26
Taritree Wongjirad <taritree@nunet.lns.mit.edu>
2016-12-13 00:00:59 -0500

    updated cua setup script

--- config/setup_cuda.sh
+++ config/setup_cuda.sh
@@ -1,3 +1,3 @@
 echo 'Setting up CUDA'
-export PATH=/usr/local/cuda-7.5/bin:${PATH}
-export LD_LIBRARY_PATH=/usr/local/cuda-7.5/lib64:${LD_LIBRARY_PATH}
+export PATH=/usr/local/cuda-8.0/bin:${PATH}
+export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:${LD_LIBRARY_PATH}
